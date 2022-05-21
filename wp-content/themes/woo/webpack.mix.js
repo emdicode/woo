1b25b38a439024f52bd31f2b0df374c939245f14
TuanNDA <tuannda@outlook.in>
2022-05-22 04:40:21 +0700

    .

---
+++ wp-content/themes/woo/webpack.mix.js
@@ -0,0 +1,27 @@
+const mix = require('laravel-mix')
+
+mix.setPublicPath('assets');
+
+mix.webpackConfig({
+    stats: {
+        children: false,
+    }
+})
+
+mix.js('src/js/main.js', './assets/main.min.js').extract();
+
+mix.postCss('src/css/main.css', './assets/main.min.css', [
+    require('postcss-import')(),
+    require('tailwindcss')('./tailwind.config.js'),
+    require('postcss-nested')(),
+    require('postcss-preset-env')(),
+    require('autoprefixer')(),
+]);
+
+if (!mix.inProduction()) {
+    mix.sourceMaps();
+}
+
+if (mix.inProduction()) {
+    mix.version();
+}
